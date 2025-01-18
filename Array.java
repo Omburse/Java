2d8a503d967df185566dbfa51310de4bf954bd6e
OM ULHAS BURSE <omburse0019@gmail.com>
2025-01-18 19:27:47 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[]args){
+        int [] marks = new int [4];
+        int n=4;
+        marks[0]=67;
+        marks[1]=90;
+        marks[2]=68;
+        marks[3]=69;
+
+    }
+}
