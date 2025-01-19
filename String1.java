8f521e92928c255091468f5bb6bd340dab4f17bf
OM ULHAS BURSE <omburse0019@gmail.com>
2025-01-19 14:37:43 +0530

    String Concept

---
+++ String1.java
@@ -0,0 +1,12 @@
+/*
+Write a java program to convert a string in toLowerCase
+ */
+
+public class String1 {
+    public static void main(String args[]){
+        String name = "BURSE";
+        String value = name.toLowerCase();
+        System.out.println(value);
+
+    }
+}
