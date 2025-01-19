2421b5ebe39653d156a5e6c5180635305f4a451d
OM ULHAS BURSE <omburse0019@gmail.com>
2025-01-19 14:38:20 +0530

    String 2 concept

---
+++ String2.java
@@ -0,0 +1,10 @@
+/*
+Write a java program to replace a Space with Undersoc
+ */
+
+public class String2 {
+    public static void main(String[] args) {
+        String name="Om Ulhas Burse";
+        System.out.println(name.replace(" ","_"));
+    }
+}
