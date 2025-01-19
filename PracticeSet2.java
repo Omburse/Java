d7685f03505340f19f1383bc98e717797898168e
OM ULHAS BURSE <omburse0019@gmail.com>
2025-01-19 14:43:50 +0530

    Practice Question For loop

---
+++ PracticeSet2.java
@@ -0,0 +1,13 @@
+/*
+Write a java program to print multiplication table of a
+given n number n.
+ */
+
+public class PracticeSet2 {
+    public static void main(String[] args) {
+        int n=7;
+        for(int i=1;i<=10;i++){
+            System.out.printf("%d X %d = %d\n",n,i,n*i);
+        }
+    }
+}
