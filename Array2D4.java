205ce4be60ae6863adeda553bfc587c6e986d8e5
OM ULHAS BURSE <omburse0019@gmail.com>
2025-01-21 13:23:20 +0530

    2D Array Problems

---
+++ Array2D4.java
@@ -0,0 +1,13 @@
+/*
+Statement--> Write a java program to reverse an array
+ */
+
+public class Array2D4 {
+    public static void main(String[] args) {
+        float [] number = { 33.76f,76.98f,80.87f,80.98f,76.77f};
+
+        for(int i=number.length-1;i>=0;i--){
+            System.out.println(number[i]);
+        }
+    }
+}
